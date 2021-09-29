04305e002d0d2b5ae4cf6433406fe5e153e3a6a6
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2021-09-29 23:16:44 +0900

    javascript dom parctice
    javascript dom parctice
    
    - html 구조 세팅

---
+++ WebP_lecture/Web_P/5w/DOMJS.js
@@ -0,0 +1,10 @@
+// document.getElementById("todo").style.display = "none";
+
+function addNPB() {
+  document.getElementById("todoli").innerHTML = "gae";
+}
+
+
+function myConvert() {
+  document.getElementById("todol").innerHTML = "Hello World";
+}

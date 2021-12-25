443c0ccd2ca8fb7b031a87837a4e6f8d0be2560d
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2021-12-25 12:11:13 +0900

    egoing_node.js practice
    생활코딩 node.js 연습

---
+++ nodejs/syntax/templete.js
@@ -0,0 +1,15 @@
+// 템플릿 리터럴
+
+var name = "hwankko";
+var letter = 'Lorem ipsum dolor sit amet,'+name+'\n\n consectetur adipisicing elit,'+name+' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+// ` ${변수명} `
+var letter = `Lorem ipsum dolor sit amet,${name}
+
+consectetur${2000+400} adipisicing elit,${name} sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.${name} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+
+
+console.log(letter);
+
+// 리터럴 : 정보롤 표현하는 방법(기호)
---
+++ nodejs/syntax/variable.js
@@ -0,0 +1,12 @@
+var a = 1;
+console.log(a);
+a = 2400;
+console.log(a);
+
+var name = "hwankko";
+var letter = 'Lorem ipsum dolor sit amet,'+name+' consectetur adipisicing elit,'+name+' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+console.log(letter);
+
+
+// 중복을 제거하면 좋은 프로그램이 됨
